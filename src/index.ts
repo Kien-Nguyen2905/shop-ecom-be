aa273a2c2b5f213e0c06ce0c29f6cdc5eb63f4cb
Daisy <kiennguyen12.com@gmail.com>
2025-07-17 15:29:55 +0700

    remove ratelimit

--- src/index.ts
+++ src/index.ts
@@ -29,14 +29,7 @@ app.use(
     crossOriginResourcePolicy: { policy: 'cross-origin' }
   })
 )
-// app.use(
-//   rateLimit({
-//     windowMs: 15 * 60 * 1000, // 15 minutes
-//     max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
-//     standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-//     legacyHeaders: false // Disable the `X-RateLimit-*` headers
-//   })
-// )
+
 app.use(
   cors({
     origin: isProduction ? env.CLIENT_URL : '*'
